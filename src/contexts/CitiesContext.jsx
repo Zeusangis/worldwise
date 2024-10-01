1466a2ad41e8b48fabd2e394800b1042af3684cf
Unish Khadka <unishk152@gmail.com>
2024-09-30 21:20:52 -0500

    updated

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -6,6 +6,7 @@ const BASE_URL = "http://localhost:9000";
 function CitiesProvider({ children }) {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
 
   useEffect(() => {
     async function fetchCities() {
@@ -21,9 +22,27 @@ function CitiesProvider({ children }) {
       }
     }
     fetchCities();
-  }, []);
+  });
+
+  async function getCity(id) {
+    useEffect(() => {
+      async function fetchCities() {
+        try {
+          setIsLoading(true);
+          const res = await fetch(`${BASE_URL}/cities/${id}`);
+          const data = await res.json();
+          setCurrentCit(data);
+        } catch {
+          alert("There was an error loading data.");
+        } finally {
+          setIsLoading(false);
+        }
+      }
+      fetchCities();
+    });
+  }
   return (
-    <CitiesContext.Provider value={{ cities, isLoading }}>
+    <CitiesContext.Provider value={{ cities, isLoading, currentCity,getCity }}>
       {children}
     </CitiesContext.Provider>
   );
